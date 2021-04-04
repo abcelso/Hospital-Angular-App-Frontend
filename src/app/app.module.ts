09de006fcbdbc436c0c3e2ff76a608ccacb04106
alrobotic <silvestroalejandro@gmail.com>
2021-04-04 13:47:56 -0300

    Diseño AdminPro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
+// Component
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Page404Component } from './pages/page404/page404.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    Page404Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    FooterComponent,
+    Grafica1Component,
+    ProgressComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]

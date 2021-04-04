7b5b475d8b75075e542366ce536e6ff5f8b47bcc
alrobotic <silvestroalejandro@gmail.com>
2021-04-04 17:26:39 -0300

    Se separa en rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 // Component
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-
     PagesComponent
   ],
   imports: [
     BrowserModule,
     PagesModule,
     SharedModule,
+    AuthModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { ProgressComponent } from './progress/progress.component';
+
+const routes: Routes = [
+  { path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: 'progress', component: ProgressComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+    ]
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class PagesroutingModule { }

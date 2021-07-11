e2e089a1b67cb94ea33f0336c9c4733b35e40078
Alejandro <silvestroalejandro@gmail.com>
2021-07-11 13:22:57 -0300

    Formulario de registro y sus validaciones

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -20,7 +20,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     RouterModule,
-    FormsModule
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,9 +10,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formSubmitted = false;
+  registerForm = this.fb.group({
+    name: ['Alejandro', [Validators.required, Validators.minLength(3)]],
+    email: ['alejandro@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', Validators.required],
+    password2: ['123456', Validators.required],
+    terms: [false, Validators.required],
+  },
+    {
+      validators: this.passwordEqual('password', 'password2')
+    }
+  );
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  createUser(): void {
+    this.formSubmitted = true;
+    if (this.registerForm.valid && !this.aceptarTerminos()){
+      console.log('Registro posteado');
+    }else{
+      console.log('Formulario no es correcto...');
+    }
+    console.log(this.registerForm);
+  }
+
+  campoNoValido(campo: string): boolean {
+    if (this.registerForm.get(campo).invalid && this.formSubmitted){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  PassNotValid(): boolean{
+    const pass1 = this.registerForm.get('password').value;
+    const pass2 = this.registerForm.get('password2').value;
+
+    if (pass1 !== pass2 && this.formSubmitted) {
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
+  aceptarTerminos(): boolean {
+    return !this.registerForm.get('terms').value && this.formSubmitted;
+  }
+
+  passwordEqual(pass1Name: string, pass2Name: string): any {
+    return (formGroup: FormGroup) => {
+      const pass1Control = formGroup.get(pass1Name);
+      const pass2Control = formGroup.get(pass2Name);
+
+      pass1Control.value === pass2Control.value ?
+        pass2Control.setErrors(null) :
+        pass2Control.setErrors({notEqual: true});
+    };
+  }
 }
